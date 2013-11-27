7bee24d768e8119f88fb32b1cae1d3cb4e023172
JP Pasala <jp@birdcar.com>
2013-11-27 00:06:36 -0800

    Working, even HTTPS is supported.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+/**
+* Copyright Jayaprakash Pasala
+* Created on 11/26/2013
+*
+* A simple proxy server using nodejs. This is a very simple proxy server. Developed this in less
+* than 24 hours. Meant to be a simple reference project or for personal use.
+*
+* Make your changes in user-agent callback to find and replace strings.
+*
+* THIS PROGRAM COMES WITH ABSOLUTELY NO WARRANTY.
+*/
 
 var net = require('net');
 var stream = require('stream');
 
-
 var DEBUG_LEVEL = {
-    ERROR: {value: 0, name: "Error"},
+    ERROR:   {value:  0, name: "Error"},
     WARNING: {value: 10, name: "Warning"},
-    INFO: {value: 20, name: "Info" },
-    FINE: {value: 30, name: "Fine" },
-    FINEST: {value:40, name: "Finest"}
+    INFO:    {value: 20, name: "Info" },
+    FINE:    {value: 30, name: "Fine" },
+    FINEST:  {value: 40, name: "Finest"}
 }
 
 var debug_level_use = 20;
@@ -26,25 +36,22 @@ var server = net.createServer(function(c) {
     var port = null;
     var client = null;
 	c.on('end', function() {
-	        if (client!=null) {
-	            client.end();
-	        } else {
-                consoleLog(DEBUG_LEVEL.INFO, 'Client is null');
-	        }
-			consoleLog(DEBUG_LEVEL.INFO, 'server disconnected');
+	    if (client!=null)
+	        client.end();
+	    else
+	        consoleLog(DEBUG_LEVEL.WARNING, 'Client is null');
+
+	    consoleLog(DEBUG_LEVEL.FINE, 'server disconnected');
 	});
 
 	c.on('data', function(data) {
 		consoleLog(DEBUG_LEVEL.INFO, 'data received');
 		var d = ''+data;
 		var indexOfSpace = d.indexOf(' ');
-		//consoleLog(DEBUG_LEVEL.FINEST, 'host ='+host);
-		if (host == null || indexOfSpace > 0) {
-		    //consoleLog(DEBUG_LEVEL.FINEST, "host is null");
-		    consoleLog(DEBUG_LEVEL.INFO, 'read data '+d);
+		if (indexOfSpace > 0) {
+		    consoleLog(DEBUG_LEVEL.FINE, 'read data '+d);
 		    var path = null;
-            //consoleLog(DEBUG_LEVEL.FINEST, 'index of space = '+indexOfSpace);
-		    if () {
+            var replacedData = d;
 			    var token = d.substring(0, indexOfSpace+1);
 			    //consoleLog(DEBUG_LEVEL.FINEST, 'token read = '+token);
 			    if (token == "GET " || token == "POST " || token == "CONNECT ") {
@@ -80,34 +87,43 @@ var server = net.createServer(function(c) {
 				    if (protocol == null)
 				        protocol = "http://";
 
-				    consoleLog(DEBUG_LEVEL.INFO, "host ="+host+" port = "+port+" protocol = "+protocol);
-			    }
+                if (token != "CONNECT ") {
+				    consoleLog(DEBUG_LEVEL.FINE, "host ="+host+" port = "+port+" protocol = "+protocol+" path ="+path);
+                    replacedData = tokens[0];
+                    replacedData = replacedData + ' '+path+' '+tokens[2]+'\n'+data.slice(headers[0].length+1);
+                }
+                else {
+                    consoleLog(DEBUG_LEVEL.FINE, "host ="+host+" port = "+port+" protocol = "+protocol+" path ="+path);
+                    replacedData = '';
+                }
 
-                consoleLog(DEBUG_LEVEL.INFO, "host ="+host+" port = "+port+" protocol = "+protocol);
-			    client = net.connect({port: port, host: host, allowHalfOpen: true},
+                consoleLog(DEBUG_LEVEL.FINE, 'writing data '+replacedData);
+
+                client = net.connect({port: port, host: host, allowHalfOpen: true},
                                 function() { //'connect' listener
-                              console.log('client connected');
-                              console.log("["+data.toString()+"]");
-                              client.write(data);
+                              if (token != "CONNECT ")
+                                client.write(replacedData);
+                              else
+                                c.write("HTTP/1.1 200 Connection established\n\n");
                             });
                             client.on('data', function(client_data) {
-                              //console.log('Data length = '+client_data.length);
                               c.write(client_data);
                             });
                             client.on('end', function() {
                               //console.log('client disconnected');
                               //c.end();
                             });
-		    }
-		} else {
-		    if (client!==null) {
+                return;
+                }
+		}
+
+		if (client!==null) {
 		        //consoleLog(DEBUG_LEVEL.INFO, "client is not null");
-		        //consoleLog(DEBUG_LEVEL.INFO, d);
+		        consoleLog(DEBUG_LEVEL.INFO, d);
 		        client.write(data);
-		    } else {
+		} else {
                 consoleLog(DEBUG_LEVEL.INFO, "client is null !!!");
                 //consoleLog(DEBUG_LEVEL.INFO, d);
-		    }
 		}
         //consoleLog(DEBUG_LEVEL.INFO, 'data end');
 	});
